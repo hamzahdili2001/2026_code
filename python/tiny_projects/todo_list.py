57ce59d3e698c3ab45dfa6895be5d10bd17bd5d2
hamzahdili2001 <hdilihamza955@gmail.com>
2026-01-29 19:21:25 +0100

    add json implementation

--- python/tiny_projects/todo_list.py
+++ python/tiny_projects/todo_list.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
 
+import json
+
+
 todos = {}
 
 
+def save_file(data):
+    with open("data.json", "w", encoding="utf-8") as write_file:
+        json.dump(data, write_file, indent=4)
+        
+def load_file():
+    with open("data.json", "r", encoding="utf-8") as read_file:
+        loaded_json = json.load(read_file)
+    
+    return loaded_json
+
+
+
+
+
 def add_task():
+    
+    global todos
+    todos = load_file() or {}
+
     while True:
         task = input("Add a task (or type 'exit'): ")
         if task == "exit":
+            save_file(todos)
             break
         
         task_id = len(todos) + 1
@@ -14,7 +36,10 @@ def add_task():
         print("Task added ✅")
 
 def view_all_tasks():
-
+    
+    global todos
+    todos = load_file() or {}
+    
     if not todos:
         print("No tasks yet.")
         return
@@ -26,6 +51,8 @@ def view_all_tasks():
 
 
 def delete_task():
+    global todos
+    todos = load_file() or {}
     view_all_tasks()
     task_id = input("Enter task number to delete: ")
 
@@ -33,6 +60,7 @@ def delete_task():
         task_id = int(task_id)
         if task_id in todos:
             todos.pop(task_id)
+            save_file(todos)
             print("Task deleted ✅")
         else:
             print("Task not found ❌")
